bc54c2401cd10a44fcaebcc1d9f282dd161d8b99
kalphageek <kalphageek@outlook.com>
2020-08-17 08:10:48 +0900

    first push

---
+++ src/test/java/me/kalplha/SpringMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kalplha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
